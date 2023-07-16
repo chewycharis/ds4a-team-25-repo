ba12c3857f72435ce7ad1bec1bc3ad98cff2fea2
chewycharis <charis_bell@hotmail.com>
2023-07-15 21:42:24 -0700

    initial draft of job reocmmender chatbot

---
+++ helper.py
@@ -0,0 +1,102 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+from PyPDF2 import PdfReader
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np 
+import pandas as pd 
+import os
+from dotenv import load_dotenv
+import openai
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+
+def suggestion_llm(clean_resume, job_description):
+  content = f"""
+  Please keep your response to 256 tokens without cut-off sentences.
+  Here is my resume: \n
+  {clean_resume} \n
+  Give me suggestions on how I can improve my resume for this job description: \n
+  {job_description}
+  """
+
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages= [{"role":"user", "content" : content}],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+
+  return response 
+
+
+
+def determine_role(user_input):
+    if "data analyst" in user_input:
+        return "data analyst"
+    if "data engineer" in user_input:
+        return "data engineer"
+    if "data scientist" in user_input:
+        return "data scientist"
+
+
+def present_jobs(top_5_jobs_df, k=0):
+    top_job = top_5_jobs_df.iloc[k]
+    print("Company Name:", top_job["Company Name"])
+    print("Job Title:", top_job["clean_job_title"])
+    print("Location:", top_job["Location"])
+    print("Industry:", top_job["Industry"])
+    print("Company Size:", top_job["Size"])
+    print("Salary Estimate ($):", top_job["Salary_Estimate_Lower_Bound"] ,"-", top_job["Salary_Estimate_Upper_Bound"])
+    print("Job Description:", top_job["clean_job_description"])
+
+def find_top_k_jobs(clean_resume, role= "data analyst", k=5):
+   if role == "data analyst":
+      job_postings = pd.read_csv("./data/data_analyst_job_postings_local.csv")
+      job_postings_embeddings = np.load("./data/data_analyst_job_postings_embeddings.npy")
+   if role =="data engineer":
+      job_postings = pd.read_csv("./data/data_engineer_job_postings_local.csv")
+      job_postings_embeddings = np.load("./data/data_engineer_job_postings_embeddings.npy")
+   if role == "data scientist": 
+      job_postings = pd.read_csv("./data/data_scientist_job_postings_local.csv")
+      job_postings_embeddings = np.load("./data/data_scientist_job_postings_embeddings.npy")
+
+   sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+   resume_embeddings = sentence_model.encode([clean_resume], show_progress_bar=False)
+   cs = cosine_similarity(job_postings_embeddings,resume_embeddings, dense_output=True)
+   top_job_post_index = cs.flatten().argsort()[::-1][:k]
+   return job_postings.iloc[top_job_post_index]
+
+
+def clean_text(text_string,lemmatize=False):
+  text_data = re.sub('[^a-zA-Z]', ' ', text_string)
+  text_data = text_data.lower()
+  text_data = text_data.split()
+  clean_text = ' '.join(text_data)
+  if lemmatize:
+    wl = WordNetLemmatizer()
+    text_data = [wl.lemmatize(word) for word in text_data if not word in set(stopwords.words('english'))]
+    text_data = ' '.join(text_data)
+    return text_data
+  else:
+    return clean_text
+  
+def get_resume_str(path):
+    pdf = PdfReader(path)
+    page = pdf.pages[0]
+    text = page.extract_text()
+    return text 
+  
+def resume_cleaning(path):
+    raw_resume = get_resume_str(path)
+    return clean_text(raw_resume, lemmatize=True)
---
+++ job_recommender.py
@@ -0,0 +1,83 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+from PyPDF2 import PdfReader
+from sentence_transformers import SentenceTransformer
+import numpy as np 
+import pandas as pd 
+
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+
+from helper import resume_cleaning, find_top_k_jobs, present_jobs, determine_role, suggestion_llm
+
+
+def main():
+    user_input_role = input("What role are you looking for? \nOur database currently have job postings for data analyst, data engineer, and data scientists roles. Please pick one.\n")
+    
+    if not any(r in user_input_role.lower() for r in ["data analyst", "data engineer", "data scientist"]):
+        print(f"Sorry, but our job postings database currently does not have postings this role.")
+        exit()
+
+    role = determine_role(user_input_role)
+    print(f"It's great that you are taking steps towards your next role!\nLet me help you find a {role} role that best fits you.")
+    raw_resume = input("To find a role that fits you, please paste a directory to your pdf resume under here:\n")
+    print("Resume text cleaning in progress ...")
+    try:
+        clean_resume = resume_cleaning(raw_resume)
+        print("Done.")
+    except:
+        print("We are having trouble parsing your resume at this time. Please ensure that your resume does not contain image or shapes.")
+        exit()
+    print("Matching resume to job postings in our database ...")
+    try:
+        top_5_jobs_df = find_top_k_jobs(clean_resume, role)
+        print("Done.")
+    except:
+        print("We are having trouble matching your resume to job postings. Come back another time.")
+        exit()
+    
+    print("Here is the first job posting we found in our data base that matches your resume the best:")
+    k=0
+    present_jobs(top_5_jobs_df,k)
+    user_input_suggestion = input("Would you like suggestions on improving your resume for this job? (Yes/No) \n")
+    if user_input_suggestion.lower() == "yes":
+        print("Suggestion generation in progress...")
+        job_description = top_5_jobs_df.iloc[k]["lemmatized_job_description"]
+        suggestion_response = suggestion_llm(clean_resume, job_description)
+        try:
+            suggestion = suggestion_response.choices[0].message.content
+            print(suggestion)
+        except:
+            print ("Sorry, something went wrong.")
+        print("\n")
+    k+=1
+    while k<5:
+        user_input_another_job = input("Would you like to see another job? (Yes/No)\n")
+        if user_input_another_job.lower() == "no":
+            print("Okay. Thank you so much for using me! Have a good day :) \n")
+            exit()
+        else:
+            print("Here is the another job posting we found in our data base that matches your resume well:")
+            present_jobs(top_5_jobs_df,k)
+            user_input_suggestion = input("Would you like suggestions on tailoring your resume to this job? (Yes/No) \n")
+            if user_input_suggestion.lower() == "yes":
+                print("Suggestion generation in progress...")
+                job_description = top_5_jobs_df.iloc[k]["lemmatized_job_description"]
+                suggestion_response = suggestion_llm(clean_resume, job_description)
+                try:
+                    suggestion = suggestion_response.choices[0].message.content
+                    print(suggestion)
+                except:
+                    print ("Sorry, something went wrong.")
+                print("\n")
+            k+=1
+    print("That was the last of the top 5 jobs!")
+    print("Thank you so much for using me! Have a good day :) \n")
+    
+
+
+if __name__ == "__main__":
+    main()
